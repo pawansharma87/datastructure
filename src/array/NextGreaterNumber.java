c167050d1eb36de37b6e0c7d918d2ace50bf35a3
Pawan Kumar <kumar.pawan@in.ibm.com>
2017-10-03 22:40:57 +0530

    Find next greater number with same set of digits

--- src/array/NextGreaterNumber.java
+++ src/array/NextGreaterNumber.java
@@ -1,5 +1,55 @@
 package array;
 
-public class NextGreaterNumber {
+import java.util.Arrays;
 
+public class NextGreaterNumber {
+	public static void main(String[] args) {
+		int digits[] = {5,3,4,9,7,6 }; // output will be 251678
+		int n = digits.length;
+		findNext(digits, n);
+	}
+
+	public static void swap(int[] digits, int i,int j) {
+		int temp = digits[i];
+		digits[i] = digits[j];
+		digits[j] = temp;
+		
+	}
+	private static void findNext(int[] digits, int n) {
+		int i;
+
+		for (i = n - 1; i > 0; i--) {
+			if (digits[i - 1] < digits[i]) {
+				break;
+			}
+		}
+		if (i == 0)
+			System.out.println("Impossible");
+		else if (i + 1 == digits.length) {
+			System.out.println("Swap last digits");
+			swap(digits, i-1, digits.length-1);
+		} else {
+			System.out.println(digits[i - 1]);
+			int j = i, k = i;
+			int min = digits[j];
+			for (; j < n; j++) {
+				if (digits[j] < min) {
+					// min=digits[j];
+					k = j;
+				}
+			}
+			System.out.println(digits[k]);
+			// Swap the above found two digits, we get 536974 in above example.
+			swap(digits, i-1, digits.length-1);
+
+			// Now sort all digits from position next to ‘d’ to the end of
+			// number.
+			Arrays.sort(digits, i, n);
+		}
+
+		for (int l = 0; l < digits.length; l++) {
+			System.out.print(digits[l] + " ");
+		}
+
+	}
 }
